6838a4e8a29a7ee97a481f4e4944b0e2ee6f3222
Kracocks <voivenelromain@gmail.com>
2025-09-04 09:26:54 +0200

    td2 manytomany

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## TD1
 
 Sur le TD1 j'ai appris a utiliser Django. Que sa soit pour créer un projet puis y ajouter une application. J'ai aussi appris à ajouter une url dans une application, avec ou sans des paramètres
+
+## TP1
+
+J'ai créer un modèle relié à une base de données avec 3 entitées et une association avec une relation many to many
--- TutoDjango/monApp/models.py
+++ TutoDjango/monApp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Categorie(models.Model):
+    idCat = models.AutoField(primary_key=True)
+    nomCat = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nomCat
+
+class Produit(models.Model):
+    refProd = models.AutoField(primary_key=True)
+    intituleProd = models.CharField(max_length=200)
+    prixUnitaireProd = models.DecimalField(max_digits=10, decimal_places=2)
+    dateFabricationProd = models.DateField(auto_now_add=True)
+    categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE, related_name="produits", null=True, blank=True)
+    rayons = models.ManyToManyField("Rayon", through="Contenir", blank=True)
+
+    def __str__(self):
+        return self.intituleProd
+
+class Rayon(models.Model):
+    idRayon = models.AutoField(primary_key=True)
+    nomRayon = models.CharField(max_length=200)
+    produits = models.ManyToManyField("Produit", through="Contenir", blank=True)
+
+    def __str__(self):
+        return self.nomRayon
+
+class Contenir(models.Model):
+    produits = models.ForeignKey(Produit, on_delete=models.CASCADE, related_name="produit")
+    rayons = models.ForeignKey(Rayon, on_delete=models.CASCADE, related_name="rayons")
+    quantite = models.IntegerField()
