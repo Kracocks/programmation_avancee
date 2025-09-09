76d34d1c097e59f60e58e99f2344d56b6badb802
Kracocks <voivenelromain@gmail.com>
2025-09-09 18:29:22 +0200

    TD2 pas fini

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ Sur le TD1 j'ai appris a utiliser Django. Que sa soit pour créer un projet puis
 ## TP1
 
 J'ai créer un modèle relié à une base de données avec 4 entitées (Produit, Rayon, Categorie, Status) et une association (Contenir) avec une relation many to many. Il est possible de voir les produits, catégories et status contenus dans la base de donnée avec les chemins /monApp/produits /monApp/categories /monApp/status.
+
+## TD2
+
+J'ai ajouté un administrateur avec la commande (python manage.py createsuperuser) et j'ai pu voir comment afficher la page administrateur et la modifier avec le fichier admin.py.
--- TutoDjango/monApp/admin.py
+++ TutoDjango/monApp/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Produit, Categorie, Status, Rayon, Contenir
 
-# Register your models here.
+class ProduitAdmin(admin.ModelAdmin):
+    model = Produit
+    list_display = ["refProd", "intituleProd", "prixUnitaireProd", "dateFabProd", "categorie", "status"]
+    list_editable = ["intituleProd", "prixUnitaireProd", "dateFabProd"]
+
+class ProduitInline(admin.TabularInline):
+    model = Produit
+    extra = 1 # nombre de lignes vides par défaut
+
+class CategorieAdmin(admin.ModelAdmin):
+    model = Categorie
+    inlines = [ProduitInline]
+
+admin.site.register(Produit, ProduitAdmin)
+admin.site.register(Categorie)
+admin.site.register(Status)
+admin.site.register(Rayon)
+admin.site.register(Contenir)
